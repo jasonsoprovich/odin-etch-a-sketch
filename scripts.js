562319b504c7337adcc449dcb0194946af7b5da5
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-01-27 19:20:16 -0700

    feat: add pseudocode for base project

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// Create a div container and append to html body
+
+// Add CSS code to assign Flexbox properties
+//   Set maximum div container size to 960px
+	
+// Declare function to build 16x16 div grid createGrid()
+//   For userInput or 16 iterations:
+//     Create square divs to create a grid
+//     Assign classes to all divs
+//     Append to div container
+			
+// Add event listeners to div grid class for hover state
+//   On mouseover change div background color
+//   On mouseout revert div background color
+
+// Declare function to reset drawing board and create new customGrid()
+//   Remove all existing grid divs in the div container
+//   Ask for userInput for new grid size
+//     Check for maximum grid size of 100x100
+//     Re-create all new divs using createGrid function with custom input
